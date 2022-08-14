2dda7206e6f6f5a8dd685a7cf0a1300f25642269
Pawel Durkalec <pdurkalec1006@gmail.com>
2022-08-14 13:09:19 +0200

    Dragging and classes initialization

---
+++ Board.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WPFChess
+{
+
+    internal static class Variables
+    {
+        public static Canvas? boardCanvas;
+    }
+
+    internal class Board
+    {
+        private Canvas boardCanvas;
+
+        private Field[,] fields = new Field[8,8];
+
+        public Board(int sizeOfField, int sizeOfOffset, int setup, Canvas boardCanvas)
+        {
+            this.initializeBoard(boardCanvas);
+            this.initalizeFields(sizeOfField, sizeOfOffset, setup);
+            
+            
+        }
+
+        private void initializeBoard(Canvas boardCanvas)
+        {
+            Variables.boardCanvas = boardCanvas;
+            Image boardImage = new Image();
+            boardImage.Source = new BitmapImage(new Uri("static/Board.jpg", UriKind.Relative));
+            Canvas.SetLeft(boardImage, 0);
+            Canvas.SetTop(boardImage, 0);
+            boardCanvas.Children.Add(boardImage);
+        }
+
+        private void initalizeFields(int sizeOfField, int sizeOfOffset, int setup)
+        {
+
+        }
+
+        private void findNearestField(int x, int y)
+        {
+
+        }
+
+
+    }
+
+    internal class Field
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        Piece piece;
+
+        Field(int x, int y, Piece piece)
+        {
+            this.x = x;
+            this.y = y;
+            this.piece = piece;
+        }
+    }
+
+    internal class Piece
+    {
+        public Image image { get; set; } = null;
+
+        public string type { get; set; }
+
+        public Field field { get; set; }
+
+
+        Piece(string imagePath, string type, Field field)
+        {
+            this.type = type;
+            this.field = field;
+            this.image = new Image();
+            image.Source = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+            Canvas.SetLeft(image, field.x);
+            Canvas.SetTop(image, field.y);
+            Variables.boardCanvas.Children.Add(this.image);
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFChess
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private double dragOffsetX = 0;
+        private double dragOffsetY = 0;
+        private object dragItem = null;
+        private Board board;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            board = new Board(116, 116, 0, boardCanvas);
+        }
+        
+        
+        private void MouseMove(object sender, MouseEventArgs e)
+        {         
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                dragItem = sender;
+                Image image = sender as Image;
+                dragOffsetX = e.GetPosition(boardCanvas).X - Canvas.GetLeft(image);
+                dragOffsetY = e.GetPosition(boardCanvas).Y - Canvas.GetTop(image);
+
+                DragDrop.DoDragDrop(image, image, DragDropEffects.Move);
+                   
+
+            }
+        }
+
+        private void Canvas_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void canvas_DragOver(object sender, DragEventArgs e)
+        {
+            Point dropPosition = e.GetPosition(boardCanvas);
+            Trace.WriteLine(sender.GetType());
+            Image image = dragItem as Image;
+            Canvas.SetLeft(image, dropPosition.X - dragOffsetX);
+            Canvas.SetTop(image, dropPosition.Y - dragOffsetY);
+        }
+    }
+}
